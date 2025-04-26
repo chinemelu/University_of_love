395fcc91b622152bc9c022850adcec48840836c4
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2025-04-26 15:55:56 +0100

    Update Github Pages

--- dist/src/views/course/index.d.ts
+++ dist/src/views/course/index.d.ts
@@ -1,2 +1,6 @@
 import "../../styles/css-reset.scss";
 import './style.scss';
+import '../../partials/navdefault';
+import '../../partials/footer';
+import '../../partials/testimonialCard';
+import '../../partials/NextSteps';
--- src/views/course/index.ts
+++ src/views/course/index.ts
@@ -2,7 +2,7 @@ import "../../styles/css-reset.scss"
 import './style.scss'
 import '../../partials/navdefault'
 import '../../partials/footer'
-import '../../partials/testimonialCard.ts'
+import '../../partials/testimonialCard'
 import '../../partials/NextSteps'
 
 
