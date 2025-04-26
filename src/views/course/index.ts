13d875ae51f66c8a139e6bfb1dcc88e30f841f4d
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2025-04-26 15:54:44 +0100

    Complete Course Page

--- src/views/course/index.ts
+++ src/views/course/index.ts
@@ -1,2 +1,35 @@
 import "../../styles/css-reset.scss"
 import './style.scss'
+import '../../partials/navdefault'
+import '../../partials/footer'
+import '../../partials/testimonialCard.ts'
+import '../../partials/NextSteps'
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const selectEl = document.querySelector('.course__select-course')
+    const optionsArray = [
+        'All',
+        'Love',
+        'Breakup',
+        'Marriage',
+        'Depression',
+        'Individual'
+    ]
+    for (let i = 0; i < optionsArray.length; i++) {
+        const el = optionsArray[i]
+        const option = document.createElement('option')
+        option.textContent = el
+        selectEl.appendChild(option)
+    }
+
+
+    const buttons = document.querySelectorAll('button') as NodeListOf<HTMLButtonElement>
+
+    Array.from(buttons).forEach(btn => {
+        (btn as HTMLButtonElement).addEventListener('click', () => {
+          window.location.href = 'course_content.html'
+        })
+    })
+})
